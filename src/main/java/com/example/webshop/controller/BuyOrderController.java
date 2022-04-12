700c5940dff305a644b4e21931d8587e433ae56f
Ali Moteirek <mtairek20@gmail.com>
2022-04-12 18:58:01 +0200

    Fixed getOrdersByCustomerId in BuyOrderController

--- src/main/java/com/example/webshop/controller/BuyOrderController.java
+++ src/main/java/com/example/webshop/controller/BuyOrderController.java
@@ -1,15 +1,10 @@
 package com.example.webshop.controller;
 
 import com.example.webshop.model.BuyOrder;
-import com.example.webshop.model.Customer;
-import com.example.webshop.model.Item;
 import com.example.webshop.repository.BuyOrderRepository;
-import com.example.webshop.repository.CustomerRepository;
-import com.example.webshop.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -17,11 +12,7 @@ import java.util.List;
 public class BuyOrderController {
 
     @Autowired
-    BuyOrderRepository buyOrderRepository;
-    @Autowired
-    CustomerRepository customerRepository;
-    @Autowired
-    ItemRepository itemRepository;
+    private BuyOrderRepository buyOrderRepository;
 
     // GET
     // http://localhost:8080/orders
@@ -29,21 +20,16 @@ public class BuyOrderController {
     public Iterable <BuyOrder> getOrders(){
         return buyOrderRepository.findAll();
     }
-    //POST
-    // Postman
-    //@PostMapping("/buy")
-    //public BuyOrder getOrdersByCustomerId (@RequestParam Long customerId, @RequestParam Long itemId){
-    //    Customer customer = customerRepository.getById(customerId);
-    //    Item item = itemRepository.getById(itemId);
-    //    BuyOrder buyOrder = BuyOrder.builder().customer(customer).items(List.of(item)).build();
-    //    return buyOrder;
-    //}
 
     // GET
     // http://localhost:8080/orders/customerId
-    //@RequestMapping("/{id}")
-    //public Iterable<BuyOrder> getOrdersByCustomerId (@PathVariable Long id){
-    //    return
-    //}
+    @RequestMapping("/{customerId}")
+    public List<BuyOrder> getOrdersByCustomerId (@PathVariable Long customerId){
+        List<BuyOrder> allByCustomerId = buyOrderRepository.findAllByCustomerId(customerId);
+        if(allByCustomerId.isEmpty()){
+            throw new RuntimeException("There are no orders available for customer with customer id: " + customerId);
+        }
+        return allByCustomerId;
+    }
 
 }
