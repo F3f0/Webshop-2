eddef589231c78423ac5caf99b7138afdc0287ca
Ali Moteirek <mtairek20@gmail.com>
2022-04-12 14:13:52 +0200

    Item Repository

--- src/main/java/com/example/webshop/repository/ItemRepository.java
+++ src/main/java/com/example/webshop/repository/ItemRepository.java
@@ -1,4 +1,9 @@
 package com.example.webshop.repository;
 
-public interface ItemRepository{
+import com.example.webshop.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
