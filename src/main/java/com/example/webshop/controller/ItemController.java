92b021ae71dcdf1d2e50cf580e8c51ad10682f86
Ali Moteirek <mtairek20@gmail.com>
2022-04-12 19:00:16 +0200

    Fixed getOrdersByCustomerId in ItemController

--- src/main/java/com/example/webshop/controller/ItemController.java
+++ src/main/java/com/example/webshop/controller/ItemController.java
@@ -1,29 +1,42 @@
 package com.example.webshop.controller;
 
+import com.example.webshop.model.BuyOrder;
+import com.example.webshop.model.Customer;
 import com.example.webshop.model.Item;
+import com.example.webshop.repository.BuyOrderRepository;
+import com.example.webshop.repository.CustomerRepository;
 import com.example.webshop.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/items")
 public class ItemController {
 
     @Autowired
-    private ItemRepository itemsRepository;
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private BuyOrderRepository buyOrderRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
 
     // GET
     // http://localhost:8080/items
     @RequestMapping
     public Iterable<Item> getAllItems() {
-        return itemsRepository.findAll();
+        return itemRepository.findAll();
     }
 
     // GET
     // http://localhost:8080/items/1
     @RequestMapping("/{id}")
     public Item getItemById(@PathVariable Long id) {
-        return itemsRepository.findById(id).orElseThrow(() ->
+        return itemRepository.findById(id).orElseThrow(() ->
                 new RuntimeException(
                         "Item with id: " + id + " could not be found"));
     }
@@ -32,8 +45,24 @@ public class ItemController {
     // Postman
     @PostMapping
     public String addNewItem(@RequestBody Item item) {
-        itemsRepository.save(item);
+        itemRepository.save(item);
         return "Saved";
     }
+
+    //POST
+    // Postman
+    @PostMapping("/buy")
+    public BuyOrder getOrdersByCustomerId (@RequestParam Long customerId, @RequestParam Long itemId){
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() ->
+                new RuntimeException(
+                        "Customer with id: " + customerId + " could not be found"));
+        Item item = itemRepository.findById(itemId).orElseThrow(() ->
+                new RuntimeException(
+                        "Item with id: " + itemId + " could not be found"));
+        BuyOrder buyOrder = BuyOrder.builder().customer(customer).items(List.of(item)).build();
+        buyOrderRepository.save(buyOrder);
+        return buyOrder;
+    }
+
 
 }
