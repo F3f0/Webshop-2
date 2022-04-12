57490b093f17a82cd8792d606b5904c6958b0806
feder <federico.barberi@gmail.com>
2022-04-12 14:15:24 +0200

    buy order repository created

--- src/main/java/com/example/webshop/repository/BuyOrderRepository.java
+++ src/main/java/com/example/webshop/repository/BuyOrderRepository.java
@@ -1,4 +1,9 @@
 package com.example.webshop.repository;
 
-public interface BuyOrderRepository {
+import com.example.webshop.model.BuyOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuyOrderRepository extends JpaRepository<BuyOrder, Long> {
 }
