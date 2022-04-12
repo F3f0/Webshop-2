500b917d7b7d306bdb9acd9aa29f397be3297b53
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-04-12 13:51:44 +0200

    Item entity created

--- src/main/java/com/example/webshop/model/Item.java
+++ src/main/java/com/example/webshop/model/Item.java
@@ -1,4 +1,33 @@
 package com.example.webshop.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Item {
+
+    @Id
+    @SequenceGenerator(
+            name = "item_sequence",
+            sequenceName = "item_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "item_sequence"
+    )
+    private Long id;
+    private String name;
+
+
+
 }
