80df8682db337f3e5fa4cb812989f44dba5b4e9c
Ali Moteirek <mtairek20@gmail.com>
2022-04-12 18:58:29 +0200

    Added findAllByCustomerId in BuyOrderRepository

--- src/main/java/com/example/webshop/repository/BuyOrderRepository.java
+++ src/main/java/com/example/webshop/repository/BuyOrderRepository.java
@@ -4,6 +4,9 @@ import com.example.webshop.model.BuyOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BuyOrderRepository extends JpaRepository<BuyOrder, Long> {
+    List<BuyOrder> findAllByCustomerId(Long customerId);
 }
